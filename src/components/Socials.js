b943de30662293c0aaed22cc2bd36bb3387cc1bf
Kristina Stout <kristinalstout@gmail.com>
2024-05-08 21:34:28 -0400

    structured additions

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import {
+  ImFacebook,
+  ImTwitter,
+  ImInstagram,
+} from 'react-icons/im'
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className = 'hidden xl:flex ml-24'>
+    <ul className = 'flex gap-x-4'>
+      <li>
+        <a href = 'http://www.facebook.com'
+        target = '_blank'>
+          <ImFacebook/>
+        </a>
+      </li>
+      <li>
+        <a href = 'http://www.twitter.com'
+        target = '_blank'>
+          <ImTwitter/>
+        </a>
+      </li>
+      <li>
+        <a href = 'https://www.instagram.com/aperturevis/'
+        target = '_blank'>
+          <ImInstagram/>
+        </a>
+      </li>
+    </ul>
+  </div>
 };
 
 export default Socials;
